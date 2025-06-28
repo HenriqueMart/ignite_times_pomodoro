38b605d58726bd05aff1fefbeac3b3accbcebb60
DevMartins <h.martins.silva2014@gmail.com>
2025-06-28 11:30:18 -0300

    Init: setting in project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from './components/Button.js'
+
+export function App() {
+
+
+  return (
+    <>
+      <Button variant='primary'/>
+      <Button variant='secondary'/>
+      <Button variant='danger'/>
+      <Button variant='sucess'/>
+      <Button/>
+    </>
+  )
+}
+
+export default App
