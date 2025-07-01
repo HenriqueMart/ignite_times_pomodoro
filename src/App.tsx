44770fc65d9b0ae0c86510d2c4240c15a4c62ef8
DevMartins <h.martins.silva2014@gmail.com>
2025-07-01 11:42:11 -0300

    Feat: add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import {ThemeProvider} from 'styled-components'
-import { Button } from './components/Button.js'
-import { defaultTheme } from './styles/themes/default.js'
-import { GlobalStyle } from './styles/global.js'
+import {ThemeProvider} from 'styled-components';
+
+import { defaultTheme } from './styles/themes/default.js';
+import { GlobalStyle } from './styles/global.js';
+
+import { Router } from './Router.js';
+import { BrowserRouter } from 'react-router-dom';
 
 export function App() {
 
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant='primary'/>
-      <Button variant='secondary'/>
-      <Button variant='danger'/>
-      <Button variant='sucess'/>
-      <Button/>
-       <GlobalStyle/>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+      <GlobalStyle/>
     </ThemeProvider>
   )
 }
