07bc4df4f43be52dabfe848c493e4a60b40c4f22
DevMartins <h.martins.silva2014@gmail.com>
2025-07-05 10:52:26 -0300

    Feat: finished use form in library

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,13 +1,36 @@
 import { Play } from "phosphor-react";
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaksInput } from "./style";
 import { useForm } from 'react-hook-form'
+import {zodResolver} from '@hookform/resolvers/zod'
+import * as zod from 'zod' // Permitindo exportar tudo da biblioteca
+
+
+const newCycleFormValidationSchema = zod.object({ //Validando objeto com esse validação nos campos
+    task : zod.string()
+        .min(1, 'Informe a Tarefa'),
+    minuteAmount: zod.
+        number()
+        .min(5, "O ciclo precisa ser de no Mínimo 5 minutos")
+        .max(60, "O ciclo precisa ser de no Máximo 60 minutos"),
+
+})
+
+type newCycleFormData = zod.infer<typeof newCycleFormValidationSchema> //Esse infer -> permite criar tipagem com base em outra variável.
+//<typeof> -> (TypeScript) Não podemos passar um variável do JS para TS, então convertemos om typeof. Esse função permite referenciar uma variável
+//Poderiamos fazer criando na mão com interface do TS, mesma coisa. 
 
 export function Home(){
-       const { register, handleSubmit, watch} = useForm();
+       const { register, handleSubmit, watch, reset} = useForm<newCycleFormData>({
+        resolver: zodResolver(newCycleFormValidationSchema), //Passando a validação dos objetos para o formulário
+        defaultValues: { // Passando como objeto os atributos que suas variável iniciadas com esses valores
+            task: '',
+            minuteAmount: 0,
+        }
+    });
 
-        function handleCreateNewCycle(data: any){  //uncontrolled 
-           console.log(data)
-
+        function handleCreateNewCycle(data: newCycleFormData){  //uncontrolled e Controlled
+           console.log(data);
+           reset(); //Voltando para os valores originais, mas só volta para o valor definido no defaultValues
         }
 
         const task = watch('task');
@@ -36,7 +59,7 @@ export function Home(){
                     <MinutesAmountInput 
                         placeholder="00" 
                         type="number" 
-                        id="minutesAmount"
+                        id="minuteAmount"
                         step={5}
                         min={0}
                         max={60}
